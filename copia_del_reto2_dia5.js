978ab1ff09c7c56c2bad23b9d72ef629102a9b53
dianacastano <diamarcast1803@gmail.com>
2022-07-19 12:58:56 +0200

    primersubida

---
+++ copia_del_reto2_dia5.js
@@ -0,0 +1,37 @@
+//////////////
+// Delaracion de Funciones
+//////////////
+
+function suma(num1, num2=0){
+
+    let resultadoSuma = num1 + num2
+    
+    return resultadoSuma
+}
+
+function resta(num1, num2=0){
+
+    let resultadoResta = num1 - num2
+    return resultadoResta
+}
+
+function producto(num1, num2=0){
+
+    let resultadoProducto = num1 * num2
+    return resultadoProducto
+    
+}
+
+function division(num1, num2=0){
+
+    let resultadoDivision = num1 / num2
+    return resultadoDivision
+}
+
+function cuadrado(num){
+    return num ** 2
+}
+
+////
+//main
+////
